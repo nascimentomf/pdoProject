d234cdc8653640bbaa8abfbb2bb6b4697a246c6b
Moises Nascimento <moises.fn@gmail.com>
2015-06-11 12:50:16 -0300

    implementando CRUD para Alunos

--- ServiceDb.php
+++ ServiceDb.php
@@ -8,7 +8,7 @@ class ServiceDB{
 		$this->db = $db;
 		$this->entidade = $entidade;
 	}
-	
+	//listagem
 	public function listar($ordem=null){
 		if($ordem){
 			$sql = "select * from {$this->entidade->getTable()} order by {$ordem}";
@@ -20,4 +20,53 @@ class ServiceDB{
 		
 		return $stmt->fetchAll(\PDO::FETCH_ASSOC);
 	}
+	
+	//novo registro
+	public function inserir(){
+		$sql = "insert into {$this->entidade->getTable()} (nome, nota) values (:nome, :nota)";
+		
+		//prepare do sql
+		$stmt = $this->db->getDb()->prepare($sql);
+		$stmt->bindValue(":nome", $this->entidade->getNome(),\PDO::PARAM_STR);
+		$stmt->bindValue(":nota", $this->entidade->getNota(),\PDO::PARAM_INT);
+		
+		//realiza cadastro de novo aluno e nota
+		if($stmt->execute()){
+			return true;
+		}
+	}
+	
+	//alterar registro existente
+	public function alterar(){
+		$sql = "update {$this->entidade->getTable()} set nome = :nome, nota = :nota where id = :id";
+		
+		//prepare do sql
+		$stmt = $this->db->getDb()->prepare($sql);
+		$stmt->bindValue(":id", $this->entidade->getid(), \PDO::PARAM_INT);
+		$stmt->bindValue(":nome", $this->entidade->getNome(), \PDO::PARAM_STR);
+		$stmt->bindValue(":nota", $this->entidade->getNota(), \PDO::PARAM_INT);
+
+		//realiza alteracao no cadastro do aluno
+		if($stmt->execute()){
+			return true;
+		}
+		
+		
+	}
+	
+	//excluir um registro
+	public function deletar($id){
+		$sql = "delete from {$this->entidade->getTable()} where id = :id";
+		
+		//prepare do sql
+		$stmt = $this->db->getDb()->prepare($sql);
+		$stmt->bindValue(":id", $id, \PDO::PARAM_INT);
+		
+		//realiza a exclusao do registro
+		if($stmt->execute()){
+			return true;
+		}
+		
+	}
+	
 }
--- index.php
+++ index.php
@@ -9,25 +9,69 @@ try{
 }catch (PDOException $e){
 	echo 'Erro ao tentar realizar conexao com banco de dados. Erro.: '.$e->getCode().'</br>';
 }
-//Exibir a listagem de alunos;
-$alunos = new Alunos();
-$alunos->setNome('Moises')
-		->setNota(8)
-;
 
-//Teste ServiceDb
+//atribuicoes de variaveis
+// acao
+if(!isset($_GET['acao'])){
+	$acao = 'listar'; 
+}else{
+	$acao = $_GET['acao'];
+}
+
+// id
+if(!isset($_GET['id'])){
+	$id = false; 
+}else{
+	$id = $_GET['id'];
+}
+
+//Entidade Aluno
+$alunos = new Alunos();
+
+//Instancia ServiceDB
 $servicedb = new ServiceDB($conexao, $alunos);
+
+// Acoes
+if($acao == 'inserir'){
+	//Inserir novo registro
+	$alunos->setNome('Caetano')
+			->setNota(5)
+	;
+	
+	//Insere no banco
+	$servicedb->inserir();	
+}
+
+if($acao == 'excluir' && $id){
+	//deletar um registro
+	$servicedb->deletar($id);
+}
+
+if($acao == 'alterar' && $id){
+	//Alterar cadastro 
+	$alunos->setId($id);
+	
+	$alunos->setNome('Moises')
+		->setNota(7)
+	;
+
+	//Altera no banco
+	$servicedb->alterar();
+}
+
 //Listagem de todos os alunos
-echo '<h3>Todos os alunos cadastrados </h3>';
-foreach ($servicedb->listar() as $resultado){
-	echo 'Nome.: '.$resultado['nome'].' -- Nota.: '.$resultado['nota'].'</br>';
+echo '<h3>Todos os alunos cadastrados | <a href="?acao=inserir">Cadastrar aluno</a></h3>';
+foreach ($servicedb->listar('nome') as $resultado){
+	echo 'Nome.: '.$resultado['nome'].' -- Nota.: '.$resultado['nota'].'-- <a href="?id='.$resultado['id'].'&acao=alterar">Alterar</a> | <a href="?id='.$resultado['id'].'&acao=excluir">Excluir</a></br>';
 }
 
 //Listagem das tres melhores notas
-echo '<h3>Melhores notas</h3>';
+echo '<h3>Tres maiores notas</h3>';
 foreach ($servicedb->listar('nota desc limit 3') as $resultado){
 	echo 'Nome.: '.$resultado['nome'].' -- Nota.: '.$resultado['nota'].'</br>';
 }
+
+
 
 /*//Teste classe aluno
 echo 'Aluno.: '.$alunos->getNome().' --> Nota.: '.$alunos->getNota().'</br>';*/
